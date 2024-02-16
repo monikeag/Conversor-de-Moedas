49bc6b5e88d1a5ce959e3ec770c63f3d0ef8df5f
Monique Aguiar <monikeagn@gmail.com>
2024-02-16 08:46:01 -0300

    alteração

--- converte.js
+++ converte.js
@@ -1,1 +1,17 @@
-console.log ("funcionou?")
+const bttConverte = document.querySelector (".bttconverte")
+
+function convertValues (){
+    const inputConverteValor = document.querySelector (".input-converte").value /* o .Value é so para chamar o valor */
+    const valorConverte = document.querySelector(".converte-um") /*Valor pra converter*/
+    const valorConvertido = document.querySelector(".converte-dois") /*Valor convertido*/
+
+    const dolarToday = 5.2
+
+    const converterValor = inputConverteValor / dolarToday
+
+    valorConverte.innerHTML = inputConverteValor
+
+    console.log(converterValor)
+}
+
+bttConverte.addEventListener ("click", convertValues)
