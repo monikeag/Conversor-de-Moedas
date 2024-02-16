750a585d02ff1da1cf860509c54f4457b6d8d980
Monique Aguiar <monikeagn@gmail.com>
2024-02-16 10:42:56 -0300

    feito

--- converte.js
+++ converte.js
@@ -1,15 +1,19 @@
 const bttConverte = document.querySelector (".bttconverte")
+const selectMoeda = document.querySelector (".select-moeda")
 
 function convertValues (){
     const inputConverteValor = document.querySelector (".input-converte").value /* o .Value é so para chamar o valor */
-    const valorConverte = document.querySelector(".converte-um") /*Valor pra converter*/
-    const valorConvertido = document.querySelector(".converte-dois") /*Valor convertido*/
+    const valorConverte = document.querySelector(".fundo") /*Valor pra converter*/
+    const valorConvertido = document.querySelector(".fundo-convertido") /*Valor convertido*/
 
+    console.log (selectMoeda.value)
     const dolarToday = 5.2
+    const euroToday = 6.2
 
-    const converterValor = inputConverteValor / dolarToday
+    const converterValor = inputConverteValor / euroToday
 
-    valorConverte.innerHTML = inputConverteValor
+    valorConverte.innerHTML = new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL"}).format (inputConverteValor)
+    valorConvertido.innerHTML = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD"}).format (converterValor) 
 
     console.log(converterValor)
 }
